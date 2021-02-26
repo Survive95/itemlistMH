b1a7cd63d07c9136bb9e88f915ae03393df30ba0
survive95 <quentinv95@outlook.com>
2021-02-26 10:07:07 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { createRef } from 'react';
+import itemlist from './itemlist.json'
 
 function App() {
+
+  const input = createRef()
+
+  const searchItem = function(){
+
+    const item = document.querySelectorAll('.itemlist_item')
+
+    for (let i = 0; i < itemlist.length; i++) {
+      const textvalue = itemlist[i].Name
+      if(textvalue.toUpperCase().indexOf(input.current.value.toUpperCase()) > -1){
+        item[i].style.display = ""
+      }
+      else{
+        item[i].style.display = "none"
+      }
+      
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main_app">
+        <form className="form_item-container">
+          <input ref={input} onChange={() => searchItem()} placeholder="Search Name" className="search_item" type="text"></input>
+        </form>
+        <table className="table_itemlist">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+          {itemlist.map((item, index) => {
+            return (
+              <tr key={index} className="itemlist_item">
+                <td>{item.ItemID}</td>
+                <td>{item.Name}</td>
+                <td>{item.Type}</td>
+              </tr>
+            )
+          })}
+        </table>
+      </main>
     </div>
   );
 }
